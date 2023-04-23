46d1e51cec5783b7322e2d8f4fbf56d12290c390
kanak <kanakjain639@gmail.com>
2023-04-23 23:38:40 +0530

    create conponents

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.get('/hello', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
 // nhi rukdekha? kya usne niche se localhost3000 liya thunder m dala then kuch kkyas laala wo usne print kraya h tum normal likh do wo kaam krega y lo isme kaage btao ohk krta haa kro y tum API bna rhe ho pta h haa haa backend kro kro aage kro okhaa
 
