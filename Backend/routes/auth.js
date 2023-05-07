827cce28cd1518f6de6ad0cf3e8bb60f78f0ac1b
kanak <kanakjain639@gmail.com>
2023-05-07 15:08:16 +0530

    create collection

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const connectToMongo = require("../db"); // y import hogya
 connectToMongo(); // y call hogya aur niche dekh database se connect bhi hogya dekh rhi h ?? haa aarha h?haa good ab models dekh kya hote h
@@ -37,10 +38,14 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exists" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      //create a new user
       user = await User.create({
         name: req.body.name, // rs mtlb server restart krne k liye m aaya 2 min
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
       res.json(user);
     } catch (error) {
