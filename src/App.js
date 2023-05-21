29664f03f12e64232c28a236a3fc24bcc17540c4
kanak <kanakjain639@gmail.com>
2023-05-21 19:30:44 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import { Home } from "./Components/Home";
+import About from "./Components/About";
 function App() {
   return (
-    <div className="App">
-      <h1>This is my iNotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
