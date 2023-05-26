22a4d0af9ff9838c4dd6e8747bb1d10af950f336
kanak <kanakjain639@gmail.com>
2023-05-26 23:14:53 +0530

    fetch notes

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,19 @@
-const connectToMongo = require('./db');
-const express = require('express')
+const connectToMongo = require("./db");
+const express = require("express");
+var cors = require("cors");
 connectToMongo();
 
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`iNotebook backend listening on port http://localhost:${port}`)
-})
+  console.log(`iNotebook backend listening on port http://localhost:${port}`);
+});
 // nhi rukdekha? kya usne niche se localhost3000 liya thunder m dala then kuch kkyas laala wo usne print kraya h tum normal likh do wo kaam krega y lo isme kaage btao ohk krta haa kro y tum API bna rhe ho pta h haa haa backend kro kro aage kro okhaa
-
